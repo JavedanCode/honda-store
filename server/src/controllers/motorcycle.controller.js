a2cb2b1ed1aae8acceb8cc129f2f84f0036c3b0d
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-06-29 15:41:41 +0300

    Add controller for getting motorcycle categories

--- server/src/controllers/motorcycle.controller.js
+++ server/src/controllers/motorcycle.controller.js
@@ -35,3 +35,17 @@ export async function getMotorcycle(req, res) {
     });
   }
 }
+
+export async function getMotorcycleCategories(req, res) {
+  try {
+    const categories = await getCategories();
+
+    res.json(categories);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
