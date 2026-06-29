545518fc33bae07a53589e94b3ee98645d6239ec
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-06-29 15:44:03 +0300

    Add routes for getting motorcycle categories and one motorcycle by slug

--- server/src/routes/motorcycle.routes.js
+++ server/src/routes/motorcycle.routes.js
@@ -1,6 +1,17 @@
 import { Router } from "express";
 
-import { getMotorcycles } from "../controllers/motorcycle.controller.js";
+import {
+  getMotorcycles,
+  getMotorcycle,
+  getMotorcycleCategories,
+} from "../controllers/motorcycle.controller.js";
+
 const router = Router();
+
 router.get("/", getMotorcycles);
+
+router.get("/categories", getMotorcycleCategories);
+
+router.get("/:slug", getMotorcycle);
+
 export default router;
