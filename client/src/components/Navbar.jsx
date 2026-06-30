a3a1058475f694b935988f77015e1c5e77b93739
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-06-30 11:33:58 +0300

    Create basic navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import hondaLogo from "../assets/Honda_Logo.svg";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <>
+      <header>
+        <img src={hondaLogo} alt="Honda" />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <a href="store">Store</a>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
