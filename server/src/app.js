039329587e960aa321db68de4e1a2f70e73abec8
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-06-30 14:27:30 +0300

    Serve images on server

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,18 @@ import cors from "cors";
 
 import motorcycleRoutes from "./routes/motorcycle.routes.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/images", express.static(path.join(__dirname, "public", "images")));
 
 app.use("/api/motorcycles", motorcycleRoutes);
 
