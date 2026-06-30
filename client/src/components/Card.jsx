6d0e3255c662622dfa1d27a8a6cafc3abd1c6ff7
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-06-30 13:54:24 +0300

    Add Card component

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card({ onClick, motorcycle }) {
+  return (
+    <div className="bike-cards" onClick={onClick}>
+      <img src={motorcycle.imagePath} alt={motorcycle.name} />
+      <h3 className="bike-names">{motorcycle.name}</h3>
+      <p className="bike-card-price">${motorcycle.price}</p>
+    </div>
+  );
+}
