53f892c9f834f03efae9d14686b670dd80fe015e
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-06-30 11:40:56 +0300

    Create basic hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,11 @@
+import heroBackground from "../assets/hero_background.jpg";
+
 export default function Hero() {
   return (
     <>
-      <img src="" alt="" />
+      <img className="hero-background-img" src={heroBackground} alt="" />
+      <p className="hero-quote">Ignite your journey with Honda</p>
+      <button className="btn explore-btn">{`[Explore Collection]`}</button>
     </>
   );
 }
