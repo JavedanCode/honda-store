4e26c22a31e7d98d9f186fbe636dc8c59dbcee45
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-06-30 14:29:07 +0300

    Fix src image path

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,11 @@
 export default function Card({ onClick, motorcycle }) {
   return (
     <div className="bike-cards" onClick={onClick}>
-      <img src={motorcycle.imagePath} alt={motorcycle.name} />
+      <img
+        width={300}
+        src={`http://localhost:3000${motorcycle.imagePath}`}
+        alt={motorcycle.name}
+      />
       <h3 className="bike-names">{motorcycle.name}</h3>
       <p className="bike-card-price">${motorcycle.price}</p>
     </div>
