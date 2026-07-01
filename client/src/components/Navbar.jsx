3a4edcd18d2f20b7f4709698c64ed21bf8e37c82
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-01 17:01:05 +0300

    Make Navbar change behaviour based on which page its on

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,11 +2,21 @@ import hondaLogo from "../assets/Honda_Logo.svg";
 import { Link } from "react-router-dom";
 import styles from "../styles/Navbar.module.css";
 
-export default function Navbar() {
+export default function Navbar({ variant = "transparent" }) {
   return (
     <>
-      <header className={styles.header}>
-        <img src={hondaLogo} alt="Honda" className={styles.logo} />
+      <header
+        className={`${styles.header} ${
+          variant === "light" ? styles.light : styles.transparent
+        }`}
+      >
+        <img
+          src={hondaLogo}
+          alt="Honda"
+          className={`${styles.logo} ${
+            variant === "light" ? styles.logoLight : styles.logoDark
+          }`}
+        />
         <nav>
           <ul className={styles.navLinks}>
             <li>
--- client/src/pages/DetailsPage.jsx
+++ client/src/pages/DetailsPage.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 export default function DetailsPage() {
   return (
     <>
-      <Navbar />
+      <Navbar variant="light" />
 
       <Details />
     </>
