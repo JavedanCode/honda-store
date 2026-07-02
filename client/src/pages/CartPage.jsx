e758a2ae4048a2f3fba6c60593403a6043a0f83f
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-02 12:41:57 +0300

    Add Cart Page

---
+++ client/src/pages/CartPage.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+
+import { useCart } from "../context/CartContext";
+
+import CartItem from "../components/CartItem";
+import CheckoutPanel from "../components/CheckoutPanel";
+
+import styles from "../styles/CartPage.module.css";
+
+export default function CartPage() {
+  const { cartItems } = useCart();
+
+  if (cartItems.length === 0) {
+    return (
+      <main className={styles.emptyCart}>
+        <h1>Your Cart is Empty</h1>
+
+        <p>Looks like you haven't added any Honda motorcycles yet.</p>
+
+        <Link to="/" className={styles.shopButton}>
+          Continue Shopping
+        </Link>
+      </main>
+    );
+  }
+
+  return (
+    <main className={styles.cartPage}>
+      <section className={styles.cartItems}>
+        <h1>Shopping Cart</h1>
+
+        {cartItems.map((item) => (
+          <CartItem key={item.motorcycle.slug} item={item} />
+        ))}
+      </section>
+
+      <CheckoutPanel />
+    </main>
+  );
+}
