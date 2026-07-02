83590ecef774189423d2337b7e45fd2df4e031ef
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-02 12:53:47 +0300

    Add Navbar to Cartpage + Add Margin to Cart Page to fix overlap with navbar

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -4,6 +4,7 @@ import { useCart } from "../context/CartContext";
 
 import CartItem from "../components/CartItem";
 import CheckoutPanel from "../components/CheckoutPanel";
+import Navbar from "../components/Navbar";
 
 import styles from "../styles/CartPage.module.css";
 
@@ -12,29 +13,35 @@ export default function CartPage() {
 
   if (cartItems.length === 0) {
     return (
-      <main className={styles.emptyCart}>
-        <h1>Your Cart is Empty</h1>
+      <>
+        <Navbar variant="light" />
+        <main className={styles.emptyCart}>
+          <h1>Your Cart is Empty</h1>
 
-        <p>Looks like you haven't added any Honda motorcycles yet.</p>
+          <p>Looks like you haven't added any Honda motorcycles yet.</p>
 
-        <Link to="/" className={styles.shopButton}>
-          Continue Shopping
-        </Link>
-      </main>
+          <Link to="/" className={styles.shopButton}>
+            Continue Shopping
+          </Link>
+        </main>
+      </>
     );
   }
 
   return (
-    <main className={styles.cartPage}>
-      <section className={styles.cartItems}>
-        <h1>Shopping Cart</h1>
+    <>
+      <Navbar variant="light" />
+      <main className={styles.cartPage}>
+        <section className={styles.cartItems}>
+          <h1>Shopping Cart</h1>
 
-        {cartItems.map((item) => (
-          <CartItem key={item.motorcycle.slug} item={item} />
-        ))}
-      </section>
+          {cartItems.map((item) => (
+            <CartItem key={item.motorcycle.slug} item={item} />
+          ))}
+        </section>
 
-      <CheckoutPanel />
-    </main>
+        <CheckoutPanel />
+      </main>
+    </>
   );
 }
