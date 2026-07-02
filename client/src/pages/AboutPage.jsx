aab1b7d8556d4a31c0e4b2addd2560f20cda1f5c
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-02 13:01:51 +0300

    Add About page with navbar

---
+++ client/src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import About from "../components/About";
+import Navbar from "../components/Navbar";
+export default function DetailsPage() {
+  return (
+    <>
+      <Navbar variant="light" />
+
+      <About />
+    </>
+  );
+}
