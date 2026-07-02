85952f0f293ba171d6beff197238f1877ba2294b
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-02 12:45:21 +0300

    Rename CheckoutPanel.css to CheckoutPanel.module.css

--- client/src/router/router.jsx
+++ client/src/router/router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import LandingPage from "../pages/LandingPage";
 import DetailsPage from "../pages/DetailsPage";
+import CartPage from "../pages/CartPage";
 
 const router = createBrowserRouter([
   {
