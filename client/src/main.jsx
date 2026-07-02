1a4126e0562dd7dd3a944928ffd593bbab105557
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-07-02 13:11:46 +0300

    Add toaster....

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,32 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { CartProvider } from "./context/CartContext.jsx";
 import router from "./router/router.jsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CartProvider>
       <RouterProvider router={router} />
+
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          duration: 2500,
+
+          style: {
+            background: "#1d1d1d",
+            color: "#fff",
+            border: "1px solid #333",
+          },
+
+          success: {
+            iconTheme: {
+              primary: "#e10600",
+              secondary: "#fff",
+            },
+          },
+        }}
+      />
     </CartProvider>
   </StrictMode>,
 );
